c57fe529872629bae44536841a2b460f195aafd7
Phu Han <phuhanld@gmail.com>
2023-09-08 20:53:28 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+const connect = async () => {
+  // make a connection to database here
+};
+
+// middlewares
+
+// hanlde status
+
+// run app
+app.listen(8080, () => {
+  connect();
+  console.log("Connected to backend.");
+});
