456b1c487294a574b386daecba0a763f730daed1
Phu Han <phuhanld@gmail.com>
2023-09-08 22:27:06 +0700

    feat(db): Configured Sequelize to connect to PostgreSQL database.

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-import express from "express";
-
+import express from 'express';
+import { Sequelize } from 'sequelize';
 const app = express();
 
-// const connect = async () => {
-//   // make a connection to database here
-// };
+const dbConfig = new Sequelize('StoreManagementSystem', 'postgres', 'postgres', {
+  host: 'localhost',
+  dialect: 'postgres',
+  operatorsAliases: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
 
-// middlewares
+const connect = async () => {
+  try {
+    await dbConfig.authenticate();
+    console.log('Connected to database.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
 
-// hanlde status
+// Middleware and routes setup
 
-// run app
-app.listen(8080, () => {
-  // connect();
-  console.log("Connected to backend.");
+// Handle status
+
+// Run app
+app.listen(8080, async () => {
+  await connect();
+  console.log('Connected to backend.');
 });
